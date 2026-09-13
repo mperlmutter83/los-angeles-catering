d273eca1b700b953cf65d12b9bd7f706c20ec460
Michael Perlmutter <yescrewnetwork@gmail.com>
2026-09-13 13:47:19 -0700

    Install Google Tag Manager (GTM-NDQGMHLR)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,8 +49,29 @@ window.dataLayer.push({
 });`,
           }}
         />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NDQGMHLR');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body className={`${playfair.variable} ${openSans.variable} font-sans antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NDQGMHLR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Header />
         <main>{children}</main>
         <Footer />
